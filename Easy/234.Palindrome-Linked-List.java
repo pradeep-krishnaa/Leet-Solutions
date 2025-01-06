c204c18b3d49da97775895df56de5d69d19da986
pradeep-krishnaa <126178098+pradeep-krishnaa@users.noreply.github.com>
2025-01-06 21:49:33 +0530

    Add files via upload

---
+++ Easy/234.Palindrome-Linked-List.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode curr = head;
+        while(curr != null){
+            list.add(curr.val);
+            curr = curr.next;
+        }
+        int left = 0;
+        int right = list.size() - 1;
+        while(left < right){
+            if(list.get(left) != list.get(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
